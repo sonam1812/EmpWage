37eb49e391d07e336fd7691e5b35e73b269660cc
sonam1812 <sonampal832@gmail.com>
2021-08-25 19:30:29 +0530

    Added: uc4

--- EmpWage.java
+++ EmpWage.java
@@ -1,27 +1,26 @@
 package com.EmpWage;
 
 public class EmpWage {
+	 public static final int IS_PART_TIME = 1;
+	    public static final int IS_FULL_TIME = 2;
+	    public static final int EMP_RATE_PER_HOUR = 20;
 	
 public static void main(String[] args) {
 	
-        double attendance = Math.floor(Math.random() * 3);
-        int WagePerHour = 20;
-        int fullDayHour = 8;
-        int DailyWage;
-        if (attendance == 1) {
-            System.out.println("Employee is Present");
-            DailyWage = WagePerHour * fullDayHour;
-            System.out.println(DailyWage);
-        }
-        else if (attendance == 2)
-        {
-            int partTimeHour = 8;
-            System.out.println("Employee is part-time employee");
-            DailyWage = WagePerHour * partTimeHour;
-            System.out.println("Employee's Daily Wage is : " +DailyWage);
-        }
-        else System.out.println("Employee is Absent");
-    }
+	 int empHrs = 0;
+     int empWage = 0;
+     int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+     switch (empCheck) {
+         case IS_PART_TIME:
+             empHrs = 4;
+             break;
+         case IS_FULL_TIME:
+             empHrs = 8;
+             break;
+         default:
+             empHrs = 0;
+     }
+     empWage = empHrs * EMP_RATE_PER_HOUR;
+     System.out.println("Emp Wage: " + empWage);
+ }
 }
-	
-
