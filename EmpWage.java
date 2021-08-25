98189a55cb1d199a72ecc436b9574e4114a80582
sonam1812 <sonampal832@gmail.com>
2021-08-25 19:17:00 +0530

    Added: program structure

--- EmpWage.java
+++ EmpWage.java
@@ -3,6 +3,11 @@ package com.EmpWage;
 public class EmpWage {
 	
 public static void main(String[] args) {
-	System.out.println("Welcome to Employee Wage");
+    double attendance = Math.floor(Math.random() * 2);
+    if (attendance == 1)
+    {
+        System.out.println("Employee is Present");
+    }
+    else System.out.println("Employee is Absent");
 }
 }
