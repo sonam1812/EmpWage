098e4169a85cdaf87e9158f27c004f069badbea2
sonam1812 <sonampal832@gmail.com>
2021-08-25 20:13:50 +0530

    Added uc6

--- EmpWage.java
+++ EmpWage.java
@@ -5,19 +5,23 @@ public class EmpWage {
 public static void main(String[] args) {
 	
 	
-        int WagePerHour = 20;
-        int fullDayHour = 8;
-        int DailyWage;
-        int i;
-        int NumberOfWorkingDays = 20;
-        int store = 0;
-        for (i=1; i<= NumberOfWorkingDays; i++)
-        {
-            double attendance = Math.floor(Math.random() * 2);
-            store += attendance;
-        }
-        System.out.println("Number of days the employee was present : "+store);
-        DailyWage = fullDayHour * WagePerHour * store;
-        System.out.println("Total Wage of Employee : "+DailyWage);
-    }
+	 int WagePerHour = 20;
+     int fullDayHour = 8;
+     int DailyWage;
+     int i;
+     int NumberOfWorkingDays = 30;
+     int store = 0;
+     for (i=1; i<= NumberOfWorkingDays; i++)
+     {
+         double attendance = Math.floor(Math.random() * 2);
+         store += attendance;
+     }
+     int totalWorkingHour = 8 * store;
+     if (totalWorkingHour >= 100)
+     {
+         DailyWage = WagePerHour * totalWorkingHour;
+         System.out.println("Employee's Wage is " +DailyWage +" Employee's Total Working Hour is " +totalWorkingHour);
+     }
+     else System.out.println("Employee's total working hour is less than 100.");
+ }
 }
